fa3502a279a3b3e3f2bb0b8f71f0bdfb902ba41c
Roca2025 <rodrigobiana@yahoo.com.ar>
2025-03-14 15:34:34 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
 // Datos de ejemplo para los productos
+// Ajusta los nombres de las imágenes según los que realmente tengas en la carpeta img/
 const products = [
   {
     id: 1,
     name: "PSA Senior 4",
     price: 640000,
-    description: "Agua purificada y segura para beber, cocinar y lavar alimentos. Diseño compacto, mayor poder de purificación y nuevo filtro Fipor® n°.3. Además, incluye kit posventa y certificación ANMAT.",
-    image: "img/psa_senior4.jpg"
+    description:
+      "Agua purificada y segura para beber, cocinar y lavar alimentos. Diseño compacto, mayor poder de purificación y nuevo filtro Fipor® n°.3. Además, incluye kit posventa y certificación ANMAT.",
+    image: "img/IMG-20250113-WA0030.jpg" // Debe coincidir con el archivo en tu carpeta
   },
   {
     id: 2,
     name: "PSA Ducha II",
     price: 209000,
-    description: "Elimina el cloro, suaviza el agua y protege la piel y el cabello. Disminuye incrustaciones y facilita la eliminación de sarro.",
-    image: "img/psa_ducha.jpg"
+    description:
+      "Elimina el cloro, suaviza el agua y protege la piel y el cabello. Disminuye incrustaciones y facilita la eliminación de sarro.",
+    image: "img/IMG-20250312-WA0004.jpg"
   },
   {
     id: 3,
     name: "SodaBurby",
     price: 307000,
-    description: "Permite obtener agua burbujeante fresca en segundos. Ideal para personalizar bebidas, ecológico y portátil.",
-    image: "img/sodaburby.jpg"
+    description:
+      "Permite obtener agua burbujeante fresca en segundos. Ideal para personalizar bebidas, ecológico y portátil.",
+    image: "img/IMG-20250313-WA0010.jpg"
   },
   {
     id: 4,
     name: "PSA MINI (Nero)",
     price: 289000,
-    description: "Purificador compacto para uso doméstico, fácil de instalar, elimina cloro e impurezas y es sustentable.",
-    image: "img/psa_mini.jpg"
+    description:
+      "Purificador compacto para uso doméstico, fácil de instalar, elimina cloro e impurezas y es sustentable.",
+    image: "img/IMG-20250204-WA0022.jpg"
   },
   {
     id: 5,
     name: "PSA Senior (Bianco)",
     price: 528000,
-    description: "Agua purificada desde la canilla con bajo costo por litro. Casquete giratorio 360° y eficiencia comprobada.",
-    image: "img/psa_senior_bianco.jpg"
+    description:
+      "Agua purificada desde la canilla con bajo costo por litro. Casquete giratorio 360° y eficiencia comprobada.",
+    image: "img/IMG-20250312-WA0003.jpg"
   },
   {
     id: 6,
     name: "PSA 1P Portátil",
     price: 86000,
-    description: "Purificador portátil ideal para viajes y camping, funciona por gravedad para obtener agua segura rápidamente.",
-    image: "img/Screenshot_20250314_152039_Facebook.jpg"  // Asegúrate de que este nombre sea correcto
+    description:
+      "Purificador portátil ideal para viajes y camping, funciona por gravedad para obtener agua segura rápidamente.",
+    image: "img/Screenshot_20250314_152039_Facebook.jpg" 
+    // Cambia aquí si le pones otro nombre al archivo
   }
 ];
 
